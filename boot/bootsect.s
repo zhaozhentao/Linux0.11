f00286852d354995cd879adf6cc93d92f48b014a
zhaozhentao <qq344696734@gmail.com>
2023-02-16 14:28:51 +0800

    改成立即数

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -7,7 +7,7 @@ _start:
 
 disable_watch_dog:
   ldr r0, =0x56000010   @ r0 存入 WATCHDOG 寄存器地址
-  mov r1, 0
+  mov r1, #0
   str r1, [r0]          @ 向 r0 所指向地址写入 r1 保存的数据，即向地址 0x56000010 写入 0，禁止 WATCHDOG
   mov pc, lr            @ 返回 
   
