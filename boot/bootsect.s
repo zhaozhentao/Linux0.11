5dbe1f3b2dc437200393784ea07d42bc5152ed83
zhaozhentao <qq344696734@gmail.com>
2023-02-16 20:58:36 +0800

    将代码复制到 sdram

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -1,12 +1,14 @@
 @ bootsect.s    (C) 2023 zhaozhentao
 
 .equ MEM_CTL_BASE, 0x48000000
+.equ SDRAM_BASE,   0x30000000
 
 .global _start
 
 _start:
   bl disable_watch_dog     @ 关闭看门狗
   bl memsetup              @ 启用外部 SDRAM
+  bl copy_to_sdram         @ 将代码复制到sdram中
 halt_loop:
     b   halt_loop          @
 
@@ -20,13 +22,23 @@ memsetup:
   mov  r1, #MEM_CTL_BASE   @ 存储控制器的13个寄存器的开始地址
   adrl r2, mem_cfg_val
   add  r3, r1, #52         @ 13 * 4, MEM_CTL_BASE 接着的第 13 个寄存器地址
-
 1:
   ldr  r4, [r2], #4        @ 读取 r2 指向的地址的值，然后 r2 加 4，指向下一个值
   str  r4, [r1], #4        @ 将 r4 的值设置到 r1 指向的地址，然后 r1 加4，指向下一个寄存器
   cmp  r1, r3              @ 比较一下看看 r1 是否已经指向最后一个需要被设置寄存器，
   bne  1b                  @ 上面的比较不成立的话继续 1: ,bne 1b 指的是 backward，倒退寻找标号为 1 的地方并跳转
   mov  pc, lr              @ 返回
+
+copy_to_sdram:
+ mov  r1, #0               @ 从 0 地址开始 
+ ldr  r2, =SDRAM_BASE      @ SDRAM 所在位置
+ mov  r3, #4*1024          @ 4k 大小
+1:
+ ldr  r4, [r1], #4         @ 将 r1 指向的地址数据读到 r4, r1 指向下一个地址
+ str  r4, [r2], #4         @ 将 r4 里面的数据复制到 r2 指向的地址，然后 r2 指向下一个地址
+ cmp  r1, r3               @ 检查复制到最后地址没
+ bne  1b                   @ 未复制到最后跳转到 1:
+ mov  pc, lr               @ 返回
 
 WATCHDOG:
   .word 0x56000010
