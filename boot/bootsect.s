325178b163e229954705baa1af34fa510e8b181d
zhaozhentao <qq344696734@gmail.com>
2023-02-16 20:44:27 +0800

    ret

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -1,7 +1,5 @@
 @ bootsect.s    (C) 2023 zhaozhentao
 
-#define ret mov pc, lr
-
 .equ MEM_CTL_BASE, 0x48000000
 
 .global _start
@@ -16,7 +14,7 @@ disable_watch_dog:
   ldr  r0, WATCHDOG        @ r0 存入 WATCHDOG 寄存器地址, 也可以直接使用 ldr r0, =0x56000010, 让编译器为这个立即数分配存放地址
   mov  r1, #0
   str  r1, [r0]            @ 向 r0 所指向地址写入 r1 保存的数据，即向地址 0x56000010 写入 0，禁止 WATCHDOG
-  ret              @ 在执行 bl 指令时，cpu 会自动将下一条指令的地址 (当前 pc + 4) 存入 lr 寄存器，通过 lr 寄存器返回 
+  mov  pc, lr              @ 在执行 bl 指令时，cpu 会自动将下一条指令的地址 (当前 pc + 4) 存入 lr 寄存器，通过 lr 寄存器返回 
   
 memsetup:
   mov  r1, #MEM_CTL_BASE   @ 存储控制器的13个寄存器的开始地址
