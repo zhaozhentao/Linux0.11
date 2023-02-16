c8f90d7f792b2e6fe7b1ab51f21746b2a537f514
zhaozhentao <qq344696734@gmail.com>
2023-02-16 14:28:09 +0800

    关闭看门狗

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -1,10 +1,13 @@
-# bootsect.s    (C) 2023 zhaozhentao
+@ bootsect.s    (C) 2023 zhaozhentao
 
 .global _start
 
 _start:
-  bl disable_watch_dog  # 关闭开门狗
+  bl disable_watch_dog  @ 关闭看门狗
 
 disable_watch_dog:
-  mov pc, lr            # 返回 
+  ldr r0, =0x56000010   @ r0 存入 WATCHDOG 寄存器地址
+  mov r1, 0
+  str r1, [r0]          @ 向 r0 所指向地址写入 r1 保存的数据，即向地址 0x56000010 写入 0，禁止 WATCHDOG
+  mov pc, lr            @ 返回 
   
