2eee6a7a9b348661a1b25d1f659116da836a2ebe
zhaozhentao <qq344696734@gmail.com>
2023-02-16 20:39:17 +0800

    memsetup

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -1,5 +1,7 @@
 @ bootsect.s    (C) 2023 zhaozhentao
 
+#define ret mov pc, lr
+
 .equ MEM_CTL_BASE, 0x48000000
 
 .global _start
@@ -14,7 +16,7 @@ disable_watch_dog:
   ldr  r0, WATCHDOG        @ r0 存入 WATCHDOG 寄存器地址, 也可以直接使用 ldr r0, =0x56000010, 让编译器为这个立即数分配存放地址
   mov  r1, #0
   str  r1, [r0]            @ 向 r0 所指向地址写入 r1 保存的数据，即向地址 0x56000010 写入 0，禁止 WATCHDOG
-  mov  pc, lr              @ 在执行 bl 指令时，cpu 会自动将下一条指令的地址 (当前 pc + 4) 存入 lr 寄存器，通过 lr 寄存器返回 
+  ret              @ 在执行 bl 指令时，cpu 会自动将下一条指令的地址 (当前 pc + 4) 存入 lr 寄存器，通过 lr 寄存器返回 
   
 memsetup:
   mov  r1, #MEM_CTL_BASE   @ 存储控制器的13个寄存器的开始地址
@@ -23,8 +25,10 @@ memsetup:
 
 1:
   ldr  r4, [r2], #4        @ 读取 r2 指向的地址的值，然后 r2 加 4，指向下一个值
-
-  mov  pc, lr
+  str  r4, [r1], #4        @ 将 r4 的值设置到 r1 指向的地址，然后 r1 加4，指向下一个寄存器
+  cmp  r1, r3              @ 比较一下看看 r1 是否已经指向最后一个需要被设置寄存器，
+  bne  1b                  @ 上面的比较不成立的话继续 1: ,bne 1b 指的是 backward，倒退寻找标号为 1 的地方并跳转
+  mov  pc, lr              @ 返回
 
 WATCHDOG:
   .word 0x56000010
