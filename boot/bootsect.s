ba023bc8dbe64ed1e6d903cd2e40cbe16cf9c6d3
zhaozhentao <qq344696734@gmail.com>
2023-02-16 14:50:57 +0800

    为立即数分配地址

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -6,8 +6,11 @@ _start:
   bl disable_watch_dog  @ 关闭看门狗
 
 disable_watch_dog:
-  ldr r0, =0x56000010   @ r0 存入 WATCHDOG 寄存器地址
+  ldr r0, WATCHDOG      @ r0 存入 WATCHDOG 寄存器地址, 也可以直接使用 ldr r0, =0x56000010, 让编译器为这个立即数分配存放地址
   mov r1, #0
   str r1, [r0]          @ 向 r0 所指向地址写入 r1 保存的数据，即向地址 0x56000010 写入 0，禁止 WATCHDOG
   mov pc, lr            @ 返回 
+  
+WATCHDOG:
+  .word 0x56000010
   
