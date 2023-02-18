f741015c800cff85ee984e65eaa2196d71bf37a7
zhaozhentao <qq344696734@gmail.com>
2023-02-18 09:27:27 +0800

    减少直接地址复制

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -2,6 +2,7 @@
 
 .equ MEM_CTL_BASE, 0x48000000
 .equ SDRAM_BASE,   0x30000000
+.equ ULCON0,       0x30000000 @ ULCON0 寄存器
 
 .global _start
 
@@ -52,26 +53,28 @@ memsetup:
 
 uart0_init:
   ldr  r0, =0x56000070        @ GPHCON 寄存器
-  mov  r1, #0xa0              @ GPH2, GPH3 用作TXD0, RXD0
-  str  r1, [r0]
-  ldr  r0, =0x56000078        @ GPHUP 寄存器
-  mov  r1, #0x0c              @ GPH2, GPH3 内部上拉
-  str  r1, [r0]
-  ldr  r0, =0x50000000        @ ULCON0 寄存器
-  mov  r1, #0x03              @ 8N1 (8 个数据位，无较验，1 个停止位)
-  str  r1, [r0]
-  ldr  r0, =0x50000004        @ UCON0 寄存器
-  mov  r1, #0x05              @ 查询方式，UART时钟源为PCLK
-  str  r1, [r0]
-  ldr  r0, =0x50000008        @ UFCON0 寄存器
-  mov  r1, #0x0               @ 不使用FIFO
-  str  r1, [r0]
-  ldr  r0, =0x5000000c        @ UMCON0 寄存器
-  mov  r1, #0x0               @ 不使用流控
-  str  r1, [r0]
-  ldr  r0, =0x50000028        @ UBRDIV0 寄存器
-  mov  r1, #26                @ 波特率为115200
-  str  r1, [r0]
+  mov  r1, #0xa0
+  str  r1, [r0]               @ GPH2, GPH3 用作TXD0, RXD0
+
+  mov  r1, #0x0c
+  str  r1, [r0, #8]           @ GPHUP 寄存器 GPH2, GPH3 内部上拉
+
+  ldr  r0, =ULCON0            @ ULCON0 寄存器
+  mov  r1, #0x03
+  str  r1, [r0]               @ 8N1 (8 个数据位，无较验，1 个停止位)
+
+  mov  r1, #0x05
+  str  r1, [r0, #4]           @ UCON0 寄存器，查询方式，UART时钟源为PCLK
+
+  mov  r1, #0x0
+  str  r1, [r0, #8]           @ UFCON0 寄存器，不使用FIFO
+
+  mov  r1, #0x0
+  str  r1, [r0, #0xc]         @ UMCON0 寄存器，配置为不使用流控
+
+  mov  r1, #26
+  str  r1, [r0, #28]          @ UBRDIV0 寄存器，配置为波特率为115200
+
   mov  pc, lr                 @ 返回
 
 print_booting_msg:
