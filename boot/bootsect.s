4b24c66b31b093da9772d09dd7a184dc90b67568
zhaozhentao <qq344696734@gmail.com>
2023-02-18 08:27:42 +0800

    调整顺序

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -22,28 +22,6 @@ disable_watch_dog:
   mov  r1, #0
   str  r1, [r0]               @ 向 r0 所指向地址写入 r1 保存的数据，即向地址 0x56000010 写入 0，禁止 WATCHDOG
   mov  pc, lr                 @ 在执行 bl 指令时，CPU 会自动将下一条指令的地址 (当前 pc + 4) 存入 lr 寄存器，通过 lr 寄存器返回 
-  
-memsetup:
-  mov  r1, #MEM_CTL_BASE      @ 存储控制器的13个寄存器的开始地址
-  adrl r2, mem_cfg_val
-  add  r3, r1, #52            @ 13 * 4, MEM_CTL_BASE 接着的第 13 个寄存器地址
-1:
-  ldr  r4, [r2], #4           @ 读取 r2 指向的地址的值，然后 r2 加 4，指向下一个值
-  str  r4, [r1], #4           @ 将 r4 的值设置到 r1 指向的地址，然后 r1 加4，指向下一个寄存器
-  cmp  r1, r3                 @ 比较一下看看 r1 是否已经指向最后一个需要被设置寄存器，
-  bne  1b                     @ 上面的比较不成立的话继续 1: ,bne 1b 指的是 backward，倒退寻找标号为 1 的地方并跳转
-  mov  pc, lr                 @ 返回
-
-copy_to_sdram:
-  mov  r1, #0                 @ 从 0 地址开始 
-  ldr  r2, =SDRAM_BASE        @ SDRAM 所在位置
-  mov  r3, #4*1024            @ 4k 大小
-1:
-  ldr  r4, [r1], #4           @ 将 r1 指向的地址数据读到 r4, r1 指向下一个地址
-  str  r4, [r2], #4           @ 将 r4 里面的数据复制到 r2 指向的地址，然后 r2 指向下一个地址
-  cmp  r1, r3                 @ 检查复制到最后地址没
-  bne  1b                     @ 未复制到最后跳转到 1:
-  mov  pc, lr                 @ 返回
 
 clock_init:
   ldr  r0, =0x4c000014        @ CLKDIVN 寄存器
@@ -59,6 +37,17 @@ clock_init:
   ldr  r1, =376850            
   str  r1, [r0]               @ 现在，FCLK=200MHz,HCLK=100MHz,PCLK=50MHz
 
+  mov  pc, lr                 @ 返回
+
+memsetup:
+  mov  r1, #MEM_CTL_BASE      @ 存储控制器的13个寄存器的开始地址
+  adrl r2, mem_cfg_val
+  add  r3, r1, #52            @ 13 * 4, MEM_CTL_BASE 接着的第 13 个寄存器地址
+1:
+  ldr  r4, [r2], #4           @ 读取 r2 指向的地址的值，然后 r2 加 4，指向下一个值
+  str  r4, [r1], #4           @ 将 r4 的值设置到 r1 指向的地址，然后 r1 加4，指向下一个寄存器
+  cmp  r1, r3                 @ 比较一下看看 r1 是否已经指向最后一个需要被设置寄存器，
+  bne  1b                     @ 上面的比较不成立的话继续 1: ,bne 1b 指的是 backward，倒退寻找标号为 1 的地方并跳转
   mov  pc, lr                 @ 返回
 
 uart0_init:
@@ -93,13 +82,24 @@ print_msg:
   add  r3, r2, #24            @ 字符串长度
 1:
   ldr  r4, [r0]               @ 读取 r0 指向的地址,即读取 UTRSTAT0 寄存器
-  tst  r4, #4
-  beq  1b
+  tst  r4, #4                 @ 检查是否发送完成
+  beq  1b                     @ 未发送完成继续检查
 
   ldrb r5, [r2], #1           @ 读取一个 byte 到 r5，r2 指向下一个 byte
-  strb r5, [r1]
+  strb r5, [r1]               @ 向 UTXH0 寄存器写入一个 byte
   cmp  r2, r3                 @ 看看 r2 地址是否已经指向 r3
   bne  1b
+  mov  pc, lr                 @ 返回
+
+copy_to_sdram:
+  mov  r1, #0                 @ 从 0 地址开始 
+  ldr  r2, =SDRAM_BASE        @ SDRAM 所在位置
+  mov  r3, #4*1024            @ 4k 大小
+1:
+  ldr  r4, [r1], #4           @ 将 r1 指向的地址数据读到 r4, r1 指向下一个地址
+  str  r4, [r2], #4           @ 将 r4 里面的数据复制到 r2 指向的地址，然后 r2 指向下一个地址
+  cmp  r1, r3                 @ 检查复制到最后地址没
+  bne  1b                     @ 未复制到最后跳转到 1:
   mov  pc, lr                 @ 返回
 
 WATCHDOG:
