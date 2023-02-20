ffb4d1ed0a0c405968865f5fd9a8b69ed606393e
zhaozhentao <qq344696734@gmail.com>
2023-02-20 21:51:56 +0800

    sdf

--- boot/head.s
+++ boot/head.s
@@ -1,6 +1,7 @@
 @ setup.s  (c) 2023 zhaozhentao
 
-#define MMU_FULL_ACCESS     (3 << 10)
+.equ MMU_SECDESC, 3090
+.equ MMU_SECDESC, 3102
 
 .global _pg_dir
 
@@ -9,5 +10,5 @@ start_up:
   bl create_page_table        @ 设置 MMU 映射
 
 create_page_table:
+  ldr r1, $MMU_SECDESC
   mov pc, lr                  @ 返回
-
