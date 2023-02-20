f0869468aba031037402863005d4ffaf58ab2a2f
zhaozhentao <qq344696734@gmail.com>
2023-02-20 20:35:57 +0800

    define

--- boot/head.s
+++ boot/head.s
@@ -1,8 +1,13 @@
 @ setup.s  (c) 2023 zhaozhentao
 
+#define MMU_FULL_ACCESS     (3 << 10)
+
 .global _pg_dir
 
 _pg_dir:
 start_up:
+  bl create_page_table        @ 设置 MMU 映射
 
+create_page_table:
+  mov pc, lr                  @ 返回
 
