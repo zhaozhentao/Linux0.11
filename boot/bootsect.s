f997160d1f080206f35ed31e91d857c1757030a8
zhaozhentao <qq344696734@gmail.com>
2023-02-22 10:01:15 +0800

    comment

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -5,7 +5,7 @@
 @ 以及将 bootsect 和 setup 模块加载到 SDRAM 中，起始地址为 0x3090000，最后跳转到 SDRAM 中执行 setup 模块
 
 .equ MEM_CTL_BASE, 0x48000000
-.equ SDRAM_BASE,   0x30090000 @ bootsect 将4K SRAM 移动到这个位置执行
+.equ SDRAM_BASE,   0x30090000 @ bootsect 将 4K SRAM 移动到这个位置执行
 .equ ULCON0,       0x30000000 @ ULCON0 寄存器
 
 .global _start
@@ -23,7 +23,7 @@ on_sdram:
   ldr pc, SETUPSEG           @ 跳转到 setup 模块
 
 disable_watch_dog:
-  ldr  r0, WATCHDOG           @ r0 存入 WATCHDOG 寄存器地址, 也可以直接使用 ldr r0, =0x56000010, 让编译器为这个立即数分配存放地址
+  ldr  r0, WATCHDOG           @ r0 存入 WATCHDOG 寄存器地址, 也可以直接使用 ldr r0, =0x53000000, 让编译器为这个立即数分配存放地址
   mov  r1, $0
   str  r1, [r0]               @ 向 r0 所指向地址写入 r1 保存的数据，即向地址 0x56000010 写入 0，禁止 WATCHDOG
   mov  pc, lr                 @ 在执行 bl 指令时，CPU 会自动将下一条指令的地址 (当前 pc + 4) 存入 lr 寄存器，通过 lr 寄存器返回 
