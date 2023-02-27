18b93286922f81427b81a278aae96cec4084c30e
zhaozhentao <qq344696734@gmail.com>
2023-02-27 17:03:46 +0800

    memory

--- init/main.c
+++ init/main.c
@@ -1,9 +1,14 @@
 #include <linux/fs.h>
 
+#define EXT_MEM_K (*(unsigned short *)0x90002)
 #define DRIVE_INFO (*(struct drive_info *)0x90080)
 #define ORIG_ROOT_DEV (*(unsigned short *)0x901FC)
 
-struct drive_info { char dummy[32]; } drive_info;
+static long memory_end = 0;
+
+struct drive_info {
+    char dummy[32];
+} drive_info;
 
 /* This really IS void, no error here. */
 void main(void) {
@@ -14,6 +19,8 @@ void main(void) {
      */
     ROOT_DEV = ORIG_ROOT_DEV;
     drive_info = DRIVE_INFO;
+    memory_end = (1<<20) + (EXT_MEM_K<<10);
+    memory_end &= 0xfffff000;
 
     while (1) {
 
