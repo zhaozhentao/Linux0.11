4d0e0b6604e90055b5fea23caa7cf6b947cf2626
zhaozhentao <qq344696734@gmail.com>
2023-02-27 16:41:14 +0800

    drive_info

--- init/main.c
+++ init/main.c
@@ -1,9 +1,19 @@
 #include <linux/fs.h>
 
+#define DRIVE_INFO (*(struct drive_info *)0x90080)
 #define ORIG_ROOT_DEV (*(unsigned short *)0x901FC)
 
+struct drive_info { char dummy[32]; } drive_info;
+
+/* This really IS void, no error here. */
 void main(void) {
+    /* The startup routine assumes (well, ...) this */
+    /*
+     * Interrupts are still disabled. Do necessary setups, then
+     * enable them
+     */
     ROOT_DEV = ORIG_ROOT_DEV;
+    drive_info = DRIVE_INFO;
 
     while (1) {
 
