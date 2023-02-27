8e31775dbcc70de2f9e5082d45df13dccc4ac44a
zhaozhentao <qq344696734@gmail.com>
2023-02-28 07:24:24 +0800

    mm

--- mm/memory.c
+++ mm/memory.c
@@ -7,6 +7,8 @@ static long HIGH_MEMORY = 0;
 #define MAP_NR(addr) (((addr)-LOW_MEM)>>12)
 #define USED 100
 
+static unsigned char mem_map[PAGING_PAGES] = {0,};
+
 void mem_init(long start_mem, long end_mem) {
     int i;
 
