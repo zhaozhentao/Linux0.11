a77c6c379c0aa621af3a0be445173e7e2cfe9ba6
zhaozhentao <qq344696734@gmail.com>
2023-03-06 13:46:09 +0800

    format

--- kernel/printk.c
+++ kernel/printk.c
@@ -3,14 +3,14 @@
 
 static char buf[1024];
 
-extern int vsprintf(char * buf, const char * fmt, va_list args);
+extern int vsprintf(char *buf, const char *fmt, va_list args);
 
 int printk(const char *fmt, ...) {
 
     va_list args;
     int i;
     va_start(args, fmt);
-	i=vsprintf(buf,fmt,args);
+    i = vsprintf(buf, fmt, args);
     va_end(args);
 
     return i;
